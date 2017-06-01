cdbed3b4983930de685fb3c7f392ece34e8e5112
Dustine Camacho <dustine.camacho@gmail.com>
2017-06-01 17:01:08 +0100

    Original mod contents (excluding some TEMP lua~ files)

---
+++ prototypes/recipe.lua
@@ -0,0 +1,36 @@
+data:extend(
+{
+  {
+    type = "recipe",
+    name = "smelt-burner-inserter",
+    category = "smelting",
+    energy_required = 3.5,
+    ingredients = {{ "burner-inserter", 1}},
+    result = "iron-plate",
+    enabled = "false",
+    hidden = "true"
+  },
+ 
+  {
+    type = "recipe",
+    name = "smelt-steel-furnace",
+    category = "smelting",
+    energy_required = 17.5,
+    ingredients = {{ "steel-furnace", 1}},
+    result = "steel-plate",
+    enabled = "false",
+    hidden = "true"
+  },
+  {
+    type = "recipe",
+    name = "smelt-burner-miner",
+    category = "smelting",
+    energy_required = 3.5,
+    ingredients = {{ "burner-mining-drill", 1}},
+    result = "iron-plate",
+    enabled = "false",
+    hidden = "true"
+
+  },
+  }
+  )
